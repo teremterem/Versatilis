9300392bc487bc0a0998ec6f98e9570ed696d7b7
Oleksandr Tereshchenko <toporok@gmail.com>
2024-05-08 14:25:30 +0300

    in progress: conversation loop

--- miniagents_copilot/agents/telegram_agent.py
+++ miniagents_copilot/agents/telegram_agent.py
@@ -2,6 +2,8 @@
 A miniagent that is connected to a Telegram bot.
 """
 
+import asyncio
+
 from miniagents.messages import Message
 from miniagents.miniagents import miniagent, InteractionContext
 from telegram import Update
@@ -35,8 +37,8 @@ async def process_telegram_update(update: Update) -> None:
         return
 
     if update.effective_message.text == "/start":
-        # TODO Oleksandr: `delegate` instead of `inquire`
-        reverse_agent.inquire(telegram_chat_id=update.effective_chat.id)
+        # the following function will not return until the conversation is over (and it is never over :D)
+        await conversation_loop(telegram_chat_id=update.effective_chat.id)
         return
 
     # TODO TODO TODO
@@ -51,12 +53,36 @@ async def process_telegram_update(update: Update) -> None:
         await update.effective_chat.send_message(str(reply))
 
 
+async def conversation_loop(telegram_chat_id: int) -> None:
+    """
+    Conversation loop between the user and the Versatilis agent.
+    """
+    # TODO Oleksandr: introduce a concept of conversation managers into the MiniAgent framework
+    user_messages = []
+    while True:
+        versatilis_message_sequence = versatilis_agent.inquire(user_messages)
+        user_messages = await user_agent.inquire(  # let's wait for the messages to avoid fast loop
+            versatilis_message_sequence, telegram_chat_id=telegram_chat_id
+        ).acollect_messages()
+
+
 @miniagent
-async def reverse_agent(_: InteractionContext, telegram_chat_id: int) -> None:
+async def user_agent(ctx: InteractionContext, telegram_chat_id: int) -> None:
     """
-    MiniAgent that starts the conversation first.
+    This is a proxy agent that represents the user in the conversation loop.
     """
-    await telegram_app.bot.send_message(telegram_chat_id, "Hello, I am a miniagent. How can I help you?")
+    async for message_promise in ctx.messages:
+        message = await message_promise.acollect()
+        await telegram_app.bot.send_message(telegram_chat_id, str(message))
+    await asyncio.sleep(5)
+
+
+@miniagent
+async def versatilis_agent(ctx: InteractionContext) -> None:
+    """
+    The main agent.
+    """
+    ctx.reply("Hello, I am Versatilis. How can I help you?")
 
 
 class TelegramUpdateMessage(Message):
